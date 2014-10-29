a2e5a83e5f810837f9532c8b73946f353f0405f5
joshsalvi <joshsalvi@JDS-MBP.local>
2014-10-29 14:08:04 -0400

    updated spike analysis

--- mkspkdens.m
+++ mkspkdens.m
@@ -11,7 +11,8 @@ function [spkdens spkbinned kernel] = mkspkdens(spkvec,tvec,thresh,sigma,plotyn)
 %
 % spkvec = 1D vector of time series data with spikes
 % tvec = time vector
-% thresh = threshold for counting spikes
+% thresh = threshold for counting spikes, if threshold is empty, it will
+% use a k-nearest-neighbors clustering algorithm to define a threshold
 % sigma = standard deviation of the kernel density estimate in units of
 % time defined by tvec. *Try different values of sigma. 
 %    - LARGE SIGMA will broaden the spike density plot
@@ -36,9 +37,15 @@ edges = [-3*sigma:dt:3*sigma];
 kernel = normpdf(edges,0,sigma)*dt;
 
 % Find the spikes using a defined threshold
-spkbinned=spkvec;
-spkbinned(spkbinned<thresh) = 0;
-spkbinned(spkbinned>=thresh) = 1;
+if isempty(thresh) == 0
+    pks = PTDetect(spkvec,thresh);
+else
+    [c1 c2] = twoclass(spkvec,1e-14);
+    thresh = max([c1 c2]);
+    pks = PTDetect(spkvec,thresh);
+end
+spkbinned=zeros(1,length(spkvec));
+spkbinned(pks)=1;
 
 % Make a spike density by convolving the binned spike data with a kernel
 spkdens = conv(spkbinned,kernel);
@@ -53,3 +60,87 @@ if plotyn == 1
     subplot(3,1,2);plot(tvec,spkvec,'k');hold on;scatter(tvec(spkbinned==1),spkvec(spkbinned==1),'r.');title('Time Series with Selected Spikes');ylabel('X');xlabel('Time');
     subplot(3,1,3);plot(tvec(1:length(spkdens)),spkdens,'r');title('Spike Density');ylabel('Density');xlabel('Time');
 end
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [P,T] = PTDetect(x, E)
+% Peak detection in data x for a given threshold E
+%
+% [P,T] = PTDetect(x, E)
+%
+% Jacobson, ML. Auto-threshold peak detection in physiological signals,
+% 2001.
+%
+% compiled: jsalvi@rockefeller.edu
+
+P=[];T=[];a=1;b=1;i=0;d=0;
+xL=length(x);
+
+while (i ~= xL)
+    i = i + 1;
+    if (d == 0)
+        if ( x(a) >= (x(i)+E) )
+            d=2;
+        elseif (x(i) >= (x(b)+E))
+            d=1;
+        end
+        if (x(a)<= x(i))
+            a = i;
+        elseif (x(i) <= x(b))
+            b = i;
+        end
+    elseif d==1
+        if (x(a)<=x(i))
+            a=i;
+        elseif (x(a) >= (x(i)+E))
+            P = [P a]; b=i; d=2;
+        end
+    elseif d==2
+        if (x(i) <= x(b))
+            b=i;
+        elseif (x(i) >= (x(b)+E))
+            T = [T b]; a = i; d=1;
+        end
+    end
+end
+
+end
+
+function [c1,c2]=twoclass(x,e)
+% Perform unsupervised learning two class separation given data x and a
+% termination condition e.
+%
+% function [c1,c2]=twoclass(x,e)
+%
+% Jacobson, ML. Auto-threshold peak detection in physiological signals,
+% 2001.
+%
+% compiled: jsalvi@rockefeller.edu
+
+c1=x(1); lastc1=c1;
+c2=x(2); lastc2=c2;
+
+while 1
+    class1=[]; class2=[];
+    for i = 1:length(x)
+        if (abs(c1-x(i)) < abs(c2-x(i)))
+            class1 = [class1 x(i)];
+        else
+            class2 = [class2 x(i)];
+        end
+    end
+    c2 = mean(class2); c1=mean(class1);
+    if (abs(lastc2-c2) < e) & (abs(lastc1-c1) < e)
+        return;
+    end
+    lastc2=c2; lastc1=c1;
+end
+
+end
+
