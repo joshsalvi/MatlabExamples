2109fecb0b96014851c4d321d408f03413f5f2de
joshsalvi <joshsalvi@MacBook-Pro.local>
2015-06-29 16:25:05 -0400

    updated motion tracking
    shows ROIs over video
    multiple ROIs

--- drosophilavideo_motiontrack.m
+++ drosophilavideo_motiontrack.m
@@ -11,10 +11,10 @@ set(0,'DefaultFigureWindowStyle','docked')
 %%%%%%%%%%%%%%%%%%%%
  
 % INPUT the filename of your video
-vid_in = '/Users/joshsalvi/Documents/Lab/Lab/Videos/Composite_(NTSC)_20150609_1454.mov';
+vid_in = '/Users/joshsalvi/Documents/Lab/Lab/Videos/Composite_(NTSC)_20150626_1313.mov';
  
 % INPUT where you'd like it to be saved.
-vid_out = '/Users/joshsalvi/Documents/Lab/Lab/Videos/Analysis-Composite_(NTSC)_20150609_1454';
+vid_out = '/Users/joshsalvi/Documents/Lab/Lab/Videos/Analysis-Composite_(NTSC)_20150626_1313';
  
 %%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%
@@ -52,25 +52,42 @@ vidmov(1:nFrames) = ...
 % Loop through each frame and extract the color data, creating a 1xn structure of HxWx3 matrices
 for i = 1:nFrames
     vidmov(i).cdata = read(vidobj,i);
+    %vidmov2(i).cdata = vidmov(i).cdata(:,:,1);
+    if mod(i,20) == 0
+        %disp(['frame ' num2str(i) '/' num2str(nFrames)]);
+        disp([num2str(round(i/nFrames*100)) '% complete']);
+    end
 end
-for j = 1:nFrames
-    vidmov2(j).cdata = vidmov(j).cdata(:,:,1);
-end
-vidmov2=vidmov;clear vidmov2
+disp([num2str(round(i/nFrames*100)) '% complete']);
+%vidmov2=vidmov;clear vidmov2
+
 % Construct a time vector (seconds)
 tvec = linspace(0,nFrames/vidobj.FrameRate,nFrames);
-ROI = ones(vidHeight,vidWidth);
+
 %% Draw an ROI (optional)
+figure(2);
+imshow(vidmov(1).cdata(:,:,1));colormap('gray');
+nroi = input('How many ROIs? ');
+setfiguredefaults(nroi);
 
 % This allows you to draw a polygon 
-ROI = roipoly(vidmov(1).cdata(:,:,1));
-figure;
-imagesc(ROI);colormap gray;
-ROI2=abs(ROI-1);
+for j = 1:nroi
+    figure(1);
+    [ROI{j}, x1{j}, y1{j}] = roipoly(vidmov(1).cdata(:,:,1));
+    figure(2);
+    %imshow(vidmov(1).cdata(:,:,1));colormap('gray');
+    hold all;
+    plot(x1{j},y1{j});hold all;
+    text(x1{j}(1),y1{j}(1),num2str(j));
+    ROI2{j}=abs(ROI{j}-1);
+end
+
 %%
 % Find the mean difference between each frame and a previous one
-for j = 2:nFrames
-    movdiff(j)=mean(mean(mean(vidmov(j).cdata(ROI==1)-vidmov(j-1).cdata(ROI==1))));
+for i = 1:nroi
+    for j = 2:nFrames
+        movdiff(i,j)=mean(mean(mean(vidmov(j).cdata(ROI{i}==1)-vidmov(j-1).cdata(ROI{i}==1))));
+    end
 end
  
 %{
@@ -78,8 +95,27 @@ end
 figure;
 plot(tvec,movdiff);
 %}
- 
+
+figure(3);
+for j = 1:nroi
+    subplot(2,ceil(nroi/2),j);
+    plot(tvec,movdiff(j,:));axis([0 tvec(end) -max(abs(movdiff(j,:))) max(abs(movdiff(j,:)))]);
+    hold on;
+    plot(tvec,movdiff(j,:)-mean(movdiff),'r');
+    title(num2str(j));
+end
+
+
+saveyn = input('Save? (1=yes):  ');
+if saveyn ==1
+disp('Saving...');
+save([vid_out '.mat'],'movdiff','ROI','tvec','vidmov','x1','y1','nFrames','ROI2');
+disp('Finished.');
+end
+
+
 %%
+j = input('Which ROI?  ');
 % Create playable movie with video AND time series [DO NOT CLICK OUT OF THE FIGURE AS IT IS PROCESSING]
 writerObj = VideoWriter(vid_out);   % create writer object (Image Processing toolbox)
 writerObj.Quality=100;
@@ -88,23 +124,24 @@ open(writerObj)                     % open the object
 
 set(0,'DefaultFigureWindowStyle','default')
 h2=figure;                          % set the figure for movie making
-spn=subplot(2,1,2); hold on;plot(tvec,movdiff,'k')  % initial plot of time series in black
+spn=subplot(2,1,2); hold on;plot(tvec,movdiff(j,:),'k')  % initial plot of time series in black
 snp = get(spn, 'pos');
 set(spn, 'Position', [0.4*snp(1) 0.15*snp(2) 1.6*snp(3) 0.8*snp(4)]);set(spn,'Yticklabel','')
 xlabel('Time (sec)');ylabel('Diff');
 axis([0 tvec(end) -0.5 12]);        % set axes [xmin xmax ymin ymax]
 for i = 1:nFrames                   % loop through each frame
 sph=subplot(2,1,1);
-%J=roifill(vidmov(i).cdata(:,:,1),ROI2);
+%J=roifill(vidmov(i).cdata(:,:,1),ROI2{j});
 J=vidmov(i).cdata(:,:,1);
 imshow(J);colormap('gray');    % plot video frame
+hold on;plot(x1{j},y1{j},'r-');
 spp = get(sph, 'pos');
 set(sph, 'Position', [1.8*spp(1) 0.9*spp(2) 0.7*spp(3) 1.4*spp(4)]);set(sph,'Xticklabel','');set(sph,'Yticklabel','');
-spn=subplot(2,1,2);plot(tvec,movdiff,'k') ;hold on;plot(tvec(1:i),movdiff(1:i),'r');    % plot time series up to this frame in red
+spn=subplot(2,1,2);plot(tvec,movdiff(j,:),'k') ;hold on;plot(tvec(1:i),movdiff(j,1:i),'r');    % plot time series up to this frame in red
 snp = get(sph, 'pos');
 set(spn, 'Position', [0.4*snp(1) 0.15*snp(2) 1.6*snp(3) 0.8*snp(4)]);set(sph,'Yticklabel','')
 xlabel('Time (sec)');ylabel('Diff');
-axis([0 tvec(end) 1.1*min(movdiff) 1.1*max(movdiff)]); 
+axis([0 tvec(end) 1.1*min(movdiff(j,:)) 1.1*max(movdiff(j,:))]); 
 h(i) = getframe(h2);                % create object/snapshot of figure frame
 writeVideo(writerObj,h(i));         % write to video object
 end
